aef52dfdd3ec0b932f2e7a96e34d3d0767bd077c
Yelyzaveta <elizaveta.kzm@gmail.com>
2019-08-23 20:59:29 +0300

    Updates

---
+++ precache-manifest.52b5b83fdc8edb4e37863c43400f2c71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7534c2d81b62c30a5857aa6851ef37e",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "aee60d609efee9f53a12",
+    "url": "/calculator/static/css/main.52208463.chunk.css"
+  },
+  {
+    "revision": "ca27c896652a217c5fc4",
+    "url": "/calculator/static/js/2.6a9f0705.chunk.js"
+  },
+  {
+    "revision": "aee60d609efee9f53a12",
+    "url": "/calculator/static/js/main.aec5b73f.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
